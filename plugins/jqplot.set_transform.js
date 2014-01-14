1f4ca8c7e077a0053e01b7a9485f959a07d9d0aa
Brian Maranville <brian.maranville@nist.gov>
2014-01-14 14:38:49 -0500

    almost done with transform setting

--- plugins/jqplot.set_transform.js
+++ plugins/jqplot.set_transform.js
@@ -1,6 +1,6 @@
 (function($) {    
     
-    function set_transform(transform, axis) {
+    function transformData(transform, axis) {
         // axis is 'xaxis' or 'x2axis' or 'yaxis' etc.
         var axis = (axis == null) ? 'yaxis' : axis;
         var axis_table = {'x': 0, 'y': 1, 'z': 2};        
@@ -11,44 +11,52 @@
             // zaxis: pass this along to series renderers
             for (var i=0; i<this.series.length; i++) {
                 var s = this.series[i];
-                if (s.set_transform) { s.set_transform(transform); }
+                if (s.set_transform) { s.set_transform(transform, axis); }
             }
         }
         else {
             // xaxis or yaxis, handle at plot level:
-            if (transform == 'log') {
-                for (var i=0; i<this.series.length; i++) {
-                    var pd = this.series[i]._plotData;
-                    //var sd = this.series[i].data;
-                    var d = this.data[i];
+            var s, pd, sd;
+            for (var i=0; i<this.series.length; i++) {
+                s = this.series[i];
+                if (s._data == null || s._data.length != s.data.length) {
+                    s._data = $.extend(true, [], s.data);
+                }
+                pd = s.data;
+                sd = s._data;
+                if (transform == 'log') {
                     for (var j=0; j<pd.length; j++) {
-                        pd[j][ax] = d[j][ax]>0 ? Math.log(d[j][ax]) / Math.LN10 : null;
+                        pd[j][ax] = sd[j][ax]>0 ? Math.log(sd[j][ax]) / Math.LN10 : null;
+                    }
+                } 
+                else { 
+                    // linear
+                    for (var j=0; j<pd.length; j++) {
+                        pd[j][ax] = sd[j][ax];
                     }
                 }
-                this.axes[axis].resetScale();
-                this.axes[axis].labelOptions.label = "log₁₀" + (this.options.axes[axis].label || "");
-                this.replot();
-            } else { // transform == 'lin'
-                for (var i=0; i<this.series.length; i++) {
-                    var pd = this.series[i]._plotData;
-                    //var sd = this.series[i].data;
-                    var d = this.data[i];
-                    for (var j=0; j<pd.length; j++) {
-                        pd[j][ax] = d[j][ax];
-                        //sd[j][1] = d[j][1];
-                    }
-                }
-                this.axes[axis].resetScale();
-                this.axes[axis].labelOptions.label = this.options.axes[axis].label || "";
-                this.replot();
             }
+            var axislabel;
+            if (transform == 'log') {
+                axislabel = "log₁₀" + (this.options.axes[axis].label || "");
+            } 
+            else {
+                axislabel = this.options.axes[axis].label || "";
+            }
+            //this.axes[axis].resetScale();
+            this.axes[axis].labelOptions.label = axislabel;
+            //this.replot();
         }
     }
     
-    function handleTransform(a,b,c,d,e) {
-        console.log(a,b,c,d,e);
-        this.set_transform = set_transform;
+    function optTransform() {
+        var options = this.options;
+        if (options.transform) {
+            for (axis in options.transform) {
+                transformData.call(this, options.transform[axis], axis); 
+            }
+        } 
     }
-    $.jqplot.postParseOptionsHooks.push(handleTransform);
+    $.jqplot.preDrawHooks.push(optTransform);
        
 })(jQuery);

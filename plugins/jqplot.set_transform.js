bf6d62cd29484a1791510d1c6c94d043b3b31b28
bmaranville <brian.maranville@nist.gov>
2014-06-26 07:21:56 -0400

    getting transform plugin ready for prime time

--- plugins/jqplot.set_transform.js
+++ plugins/jqplot.set_transform.js
@@ -10,11 +10,13 @@
         var axlower = axis[0] + 'lower'; // for error bars
         this['_' + axis + '_transform'] = transform;
         if (ax == 2) { 
-            // zaxis: pass this along to series renderers
+            // zaxis: do nothing
+            /*
             for (var i=0; i<this.series.length; i++) {
                 var s = this.series[i];
                 if (s.set_transform) { s.set_transform(transform, axis); }
             }
+            */
         }
         else {
             // xaxis or yaxis, handle at plot level:
@@ -30,32 +32,24 @@
                     for (var j=0; j<pd.length; j++) {
                         pd[j][ax] = sd[j][ax]>0 ? Math.log(sd[j][ax]) / Math.LN10 : null;
                         if (pd[j].length > 2) {
-                            pd[j][2][axupper] = d[j][2][axupper]>0 ? Math.log(d[j][2][axupper]) / Math.LN10 : null;
-                            pd[j][2][axlower] = d[j][2][axlower]>0 ? Math.log(d[j][2][axlower]) / Math.LN10 : null;
+                            pd[j][2][axupper] = sd[j][2][axupper]>0 ? Math.log(sd[j][2][axupper]) / Math.LN10 : null;
+                            pd[j][2][axlower] = sd[j][2][axlower]>0 ? Math.log(sd[j][2][axlower]) / Math.LN10 : null;
                         }
                     }
+                    
                 } 
                 else { 
                     // linear
                     for (var j=0; j<pd.length; j++) {
                         pd[j][ax] = sd[j][ax];
                         if (pd[j].length > 2) {
-                            pd[j][2][axupper] = d[j][2][axupper];
-                            pd[j][2][axlower] = d[j][2][axlower];
+                            pd[j][2][axupper] = sd[j][2][axupper];
+                            pd[j][2][axlower] = sd[j][2][axlower];
                         }
                     }
                 }
             }
-            var axislabel;
-            if (transform == 'log') {
-                axislabel = "log₁₀" + (this.options.axes[axis].label || "");
-            } 
-            else {
-                axislabel = this.options.axes[axis].label || "";
-            }
-            //this.axes[axis].resetScale();
-            this.axes[axis].labelOptions.label = axislabel;
-            //this.replot();
+            this.axes[axis].resetScale();
         }
     }
     
@@ -67,6 +61,37 @@
             }
         } 
     }
-    $.jqplot.preDrawHooks.push(optTransform);
+
+    $.jqplot.postParseOptionsHooks.push(optTransform);
+    
+    function annotateLabel(transform, axis) {
+        // axis is 'xaxis' or 'x2axis' or 'yaxis' etc.
+        var axis = (axis == null) ? 'yaxis' : axis;
+        var axis_table = {'x': 0, 'y': 1, 'z': 2};        
+        if (!(axis[0] in axis_table)) { return }
+        var ax = axis_table[axis[0]]; // based on first character of axis name
+        if (ax < 2) { // x or y
+            var axislabel;
+            if (transform == 'log') {
+                axislabel = "log₁₀" + (this.options.axes[axis].label || "");
+            } 
+            else {
+                axislabel = this.options.axes[axis].label || "";
+            }
+            this.axes[axis].labelOptions.label = axislabel;            
+        }
+    }
+    
+    
+    function updateTransformLabels() {
+        var options = this.options;
+        if (options.transform) {
+            for (axis in options.transform) {
+                annotateLabel.call(this, options.transform[axis], axis); 
+            }
+        }
+    }
+    
+    $.jqplot.preDrawHooks.push(updateTransformLabels);
        
 })(jQuery);
